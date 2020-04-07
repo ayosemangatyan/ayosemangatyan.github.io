31fca960d50b76ba2f6cbed55beec756ec6892c1
ayosemangatyan <liankerja7@gmail.com>
2020-04-07 20:48:56 +0700

    Menyalin isi folder build

---
+++ precache-manifest.a12241d69bb6be0c41098b5fb78b49bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "956bcb607057c9df8828",
+    "url": "/static/js/main.956bcb60.chunk.js"
+  },
+  {
+    "revision": "f31dbe5aead16274bb03",
+    "url": "/static/js/1.f31dbe5a.chunk.js"
+  },
+  {
+    "revision": "956bcb607057c9df8828",
+    "url": "/static/css/main.256e2eb1.chunk.css"
+  },
+  {
+    "revision": "8d7ad4c3e979321b3393c8cffe0bd959",
+    "url": "/index.html"
+  }
+];
